479bc21ebccc5c0156c0e9ac7301a89f881310f3
Bruja <sebaspe12345@gmail.com>
2020-03-12 17:47:38 -0500

    carrera

--- src/controllers/controller_certifi.js
+++ src/controllers/controller_certifi.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     certificado = require('../models/model_certifi'),
     carrera = require('../models/model_carrer')
-    router = express.Router();
+router = express.Router();
 
 router.post('/newCertifi', (req, res) => {
     var body = req.body;
@@ -15,21 +15,25 @@ router.post('/newCertifi', (req, res) => {
         parroquia: body.parroquia,
         canton: body.canton,
         horas: body.horas,
-        date_inicio:body.date_inicio,
-        date_fin:body.date_fin
-    }, (err,rest) => {
+        date_inicio: body.date_inicio,
+        date_fin: body.date_fin
+    }, (err, rest) => {
         if (err) {
             console.error(err)
             throw err;
         }
         res.status(200).json(rest)
     })
-}).post('/searchCertifi',(req,res)=>{
-    certificado.find({codigo:req.body.codigo},(err,rest)=>{
-        rest.forEach(data=>{
-            carrera.find({id:data.name_carrer},(err,rest1)=>{
-                rest1.forEach(data2=>{
-                    data.name_carrer=data2.nameCarrer                   
+}).post('/searchCertifi', (req, res) => {
+    var car;
+    certificado.find({ codigo: req.body.codigo }, (err, rest) => {
+        rest.forEach(data => {
+            carrera.find({ id: data.name_carrer }, (err, rest1) => {
+                rest1.forEach(data2 => {
+                    if (data.name_carrer == data2.id) {
+                        data.name_carrer = data2.nameCarrer;
+                        res.status(200).json(rest)
+                    }
                 })
             })
         })
@@ -37,7 +41,6 @@ router.post('/newCertifi', (req, res) => {
             console.error(err)
             throw err;
         }
-        res.status(200).json(rest)
     })
 })
 
