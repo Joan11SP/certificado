3fe342ea9fd86665d422fec70b3937f1bb30b6cb
Bruja <sebaspe12345@gmail.com>
2020-03-12 16:55:10 -0500

    hoy

--- src/conection-mongo/dbconection.js
+++ src/conection-mongo/dbconection.js
@@ -1,7 +1,7 @@
 const mongo = require('mongoose');
       
-//var connection = mongo.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Certificado?retryWrites=true&w=majority');
-var connection = mongo.connect('mongodb://localhost:27017/Certificado');
+var connection = mongo.connect('mongodb+srv://Bruja:1925BscJp@cluster0-hp4pe.mongodb.net/Certificado?retryWrites=true&w=majority');
+//var connection = mongo.connect('mongodb://localhost:27017/Certificado');
 
 
 mongo.connection.on('open',ref=>{
