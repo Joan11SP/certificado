4296d9834610ac4147d21df4b26611292760de91
Bruja <sebaspe12345@gmail.com>
2020-03-13 13:46:05 -0500

    posi

--- src/controllers/controller_identities.js
+++ src/controllers/controller_identities.js
@@ -2,12 +2,3 @@ const express = require('express'),
     identidades = require('../models/model_identities'),    
     router = express.Router();
 
-router.post('/marcoIdentities',async (req, res) => {
-    const image=new identidades();
-    image.imageISTL = req.body.imageISTL;
-    image.name = req.body.name
-    console.log(image)
-    await image.save();
-})
-
-module.exports = router;
