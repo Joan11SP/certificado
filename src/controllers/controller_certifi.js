f07ae274a142d68eb0b66a8e674970517319bfa6
Bruja <sebaspe12345@gmail.com>
2020-03-19 13:14:40 -0500

    ahoramaldita

--- src/controllers/controller_certifi.js
+++ src/controllers/controller_certifi.js
@@ -7,31 +7,34 @@ const express = require('express'),
 router.post('/newCertifi', (req, res) => {
     var body = req.body;
     if (validar(body.dni) === true) {
-        certificado.insertMany({
-            codigo: body.codigo,
-            names: body.names,
-            dni: body.dni,
-            name_carrer: body.id,
-            name_project: body.name_project,
-            barrio: body.barrio,
-            parroquia: body.parroquia,
-            canton: body.canton,
-            horas: body.horas,
-            date_inicio: body.date_inicio,
-            date_fin: body.date_fin
-        }, (err, rest) => {
-            if (err) {
-                console.error(err)
-                throw err;
+        certificado.find({dni:body.dni},(err,rest1)=>{
+            if(rest1.length!==1){
+                certificado.insertMany({
+                    codigo: body.codigo,
+                    names: body.names,
+                    dni: body.dni,
+                    name_carrer: body.id,
+                    name_project: body.name_project,
+                    barrio: body.barrio,
+                    parroquia: body.parroquia,
+                    canton: body.canton,
+                    horas: body.horas,
+                    date_inicio: body.date_inicio,
+                    date_fin: body.date_fin
+                }, (err, rest) => {
+                    if (err) {
+                        console.error(err)
+                        throw err;
+                    }
+                    res.status(200).json(rest)
+                })
             }
-            res.status(200).json(rest)
+            else{
+                res.json({ mensaje: "cedula_existe" })
+            }
         })
     } else {
         res.json({ mensaje: "cedula_incorrecta" })
-    }
-    if (err) {
-        res.json(rest);
-        throw err;
     }
 }).post('/searchCertifi', (req, res) => {
     certificado.find({ codigo: req.body.codigo }, (err, rest) => {
