4e768536e278e024ffe76b3e781342e87e52e05d
Bruja <sebaspe12345@gmail.com>
2020-03-25 16:40:38 -0500

    update

--- src/controllers/controller_certifi.js
+++ src/controllers/controller_certifi.js
@@ -8,7 +8,7 @@ router = express.Router();
 router.post('/newCertifi', (req, res) => {
     var body = req.body;
     if (validar(body.dni) === true) {
-        certificado.find({ codigo: body.codigo,status: { $in: [1] } }, (err, rest) => {
+        certificado.find({ codigo: body.codigo}, (err, rest) => {
             if (rest.length !== 1) {
                 certificado.insertMany({
                     codigo: body.codigo,
@@ -40,7 +40,7 @@ router.post('/newCertifi', (req, res) => {
         res.json({ mensaje: "cedula_incorrecta" })
     }
 }).get('/searchCertifi', (req, res) => {
-    certificado.find({}, (err, rest) => {
+    certificado.find({status: { $in: [1] } }, (err, rest) => {
         if (rest.length === 0) {
             res.json(rest);
         }
@@ -49,7 +49,7 @@ router.post('/newCertifi', (req, res) => {
         }
     })
 }).post('/searchOneCertifi', (req, res) => {
-    certificado.find({codigo:req.body.codigo}, (err, rest) => {
+    certificado.find({codigo:req.body.codigo,status: { $in: [1] } }, (err, rest) => {
         if (rest.length === 0) {
             res.json(rest);
         }
@@ -96,7 +96,7 @@ router.post('/newCertifi', (req, res) => {
         res.json({ mensaje: "cedula_incorrecta" })
     }
 }).post('/deleteCertifi', (req, res) => {
-    certificado.updateOne({ codigo: req.body.codigo,
+    certificado.update({ codigo: req.body.codigo},{
         $set: { status: 0 } }, (err, docs) => {
             if (err) {
                 console.error(err);
